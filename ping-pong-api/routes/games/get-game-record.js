c9a671d5cfce7cfb1a8692f9deca5444e090a65d
Cody Evins <cody.evins@gmail.com>
2025-11-25 11:00:23 -0600

    add wins & losses to game report player data

--- ping-pong-api/routes/games/get-game-record.js
+++ ping-pong-api/routes/games/get-game-record.js
@@ -62,12 +62,32 @@ async function getGameRecord (gameId, res) {
         THEN MAX(playerOneScore - playerTwoScore)
         WHEN servingPlayer = g.playerTwoId
         THEN MAX(playerTwoScore - playerOneScore)
-      END AS largestLead
+      END AS largestLead,
+      w.wins,
+      l.losses
     FROM points p
     JOIN games g 
       ON p.gameId = g.id
     JOIN players pl 
       ON p.servingPlayer = pl.id
+    LEFT JOIN (
+			SELECT
+				winnerId AS playerId,
+				COUNT(*) AS wins
+			FROM Games 
+			GROUP BY
+				winnerId
+		) w
+			ON pl.id = w.playerId
+		LEFT JOIN (
+			SELECT
+				loserId AS playerId,
+				COUNT(*) AS losses
+			FROM Games 
+			GROUP BY 
+				loserId
+		) l 
+			ON pl.id = l.playerId
     WHERE p.gameId = ${gameId}
     GROUP BY
       servingPlayer;
